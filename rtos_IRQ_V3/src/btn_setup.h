6a20a4eeb63b5a0d928212bac80e19ea18eaff6e
ainoal <77739788+ainoal@users.noreply.github.com>
2023-12-08 10:42:53 +0200

    Revert "UART and button semaphores implemented"

--- rtos_IRQ_V3/src/btn_setup.h
+++ rtos_IRQ_V3/src/btn_setup.h
@@ -3,7 +3,6 @@
 #include "zynq_registers.h"
 #include "pi_controller.h"
 #include "state_machine.h"
-//#include "semphr.h"
 
 #ifndef SRC_BTN_SETUP_H_
 #define SRC_BTN_SETUP_H_
@@ -11,15 +10,11 @@
 #define BUTTONS_channel		2
 #define BUTTONS_AXI_ID		XPAR_AXI_GPIO_SW_BTN_DEVICE_ID
 
-//extern XScuGic xInterruptController;	// Interrupt controller instance
+extern XScuGic xInterruptController;	// Interrupt controller instance
 
-//extern SemaphoreHandle_t button_semaphore;
-
-//void init_button_semaphore(void)
 void SetupPushButtons();
-void PushButtons_Handler();
+void PushButtons_Intr_Handler();
 int checkUARTSemaphoreStatus(void);
-int take_button_semaphore(void);
 
 
 #endif /* SRC_BTN_SETUP_H_ */
