5f7afef7287fabff752dd634d16f1a7e05001c55
ainoal <77739788+ainoal@users.noreply.github.com>
2023-12-21 16:31:54 +0200

    Delete RTOS_converter_V2 directory

--- RTOS_converter_V2/Debug/src/pi_controller.d
+++
@@ -1,4 +0,0 @@
-src/pi_controller.o src/pi_controller.o: ../src/pi_controller.c \
- ../src/pi_controller.h
-
-../src/pi_controller.h:
--- RTOS_converter_V2/Release/src/converter_model.d
+++
@@ -1,4 +0,0 @@
-src/converter_model.o src/converter_model.o: ../src/converter_model.c \
- ../src/converter_model.h
-
-../src/converter_model.h:
